3a4d9642543ba66da56ba45d62329c6b446eca27
jinwoo.choi <jinwoo.choi@aichemist.kr>
2025-08-03 13:48:57 +0900

    원격서버 ncp로 롤백

--- iot_dashbord/lib/constants/global_constants.dart
+++ iot_dashbord/lib/constants/global_constants.dart
@@ -7,8 +7,8 @@
 /// 공통 API 주소들 (상용)
 const String baseUrl3030 = 'https://hanlimtwin.kr:3030'; // AlarmHistory 등
 const String baseUrl4040 = 'https://hanlimtwin.kr:4040'; // CCTV 관련
-//const String baseUrl5002 = 'https://hanlimtwin.kr:5002'; // CCTV 모션 관련 (상용 1, 지금은 쓰이지 않음)
-const String baseUrl5002 = 'http://14.46.223.172:5002'; // 상용 2
+const String baseUrl5002 = 'https://hanlimtwin.kr:5002'; // CCTV 모션 관련 (상용 1, 지금은 쓰이지 않음)
+//const String baseUrl5002 = 'http://14.46.223.172:5002'; // 상용 2
 
 
 
