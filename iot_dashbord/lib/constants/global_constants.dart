7de1fca33a6a9ada7ae5ff7f68c2a9ecaffe59e6
wwf57755 <wwf57755@gmail.com>
2025-08-03 11:58:53 +0900

    한림 원격으로 파이썬 서버 돌리기

--- iot_dashbord/lib/constants/global_constants.dart
+++ iot_dashbord/lib/constants/global_constants.dart
@@ -8,7 +8,7 @@
 const String baseUrl3030 = 'https://hanlimtwin.kr:3030'; // AlarmHistory 등
 const String baseUrl4040 = 'https://hanlimtwin.kr:4040'; // CCTV 관련
 // const String baseUrl5002 = 'https://hanlimtwin.kr:5002'; // CCTV 모션 관련 (현재는 아래꺼 쓰임)
-const String baseUrl5002 = 'http://14.46.223.172:5002'
+const String baseUrl5002 = 'http://14.46.223.172:5002';
 
 // 로컬 테스트용
 //  const String baseUrl3030 = 'http://localhost:3030'; // AlarmHistory 등
